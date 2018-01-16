baa66f32c2bc6e446c6cf225c95391d22387c48f
Hristo Filaretov <h.filaretov@campus.tu-berlin.de>
2018-01-16 16:14:26 +0100

    remove redundant =

--- fish/.config/fish/functions/git_origin_url.fish
+++ fish/.config/fish/functions/git_origin_url.fish
@@ -1,5 +1,5 @@
 function git_origin_url --description 'Echo current git repo name'
-    set -l git_url = (git config --get remote.origin.url)
+    set -l git_url (git config --get remote.origin.url)
     if test $status -eq 0
         echo (basename "$git_url" ".git")
     end
