37acb062c19b9928db9288532672e96872e567f3
Hristo Filaretov <hristo.filaretov@ipk.fraunhofer.de>
2018-04-04 19:07:34 +0200

    fish: cleaned up local stuff

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,5 +1,6 @@
 set -g theme_color_scheme base16-light
 set -x FZF_DEFAULT_COMMAND 'rg --files'
+set  fish_greeting "Don't panic!"
 
 alias install "sudo dnf install"
 alias search "dnf search"
@@ -10,13 +11,10 @@ alias gacm "git add .; and git commit -m"
 alias v "nvim"
 alias add_path "set fish_user_paths $fish_user_paths"
 
-set -x PATH $PATH ~/.local/bin
-set -x PATH $PATH ~/.arm/bin
-
 # Find local fish file
-set lc_file "~/.config/fish"(hostname)".fish"
+set -l lc_file "$HOME/.config/fish/"(hostname)".fish"
 if test -e $lc_file
     source $lc_file
 else
-    echo "No local fish file"
+    echo "No local fish file in "(pwd)" $lc_file"
 end
