462245f5d5e7238eb6dd75b8797277632498090d
Hristo Filaretov <hristo.filaretov@ipk.fraunhofer.de>
2018-04-05 08:31:30 +0200

    fish: clean up ipk.fish

--- fish/.config/fish/fedora.ipk.fish
+++ fish/.config/fish/fedora.ipk.fish
@@ -1,7 +1,3 @@
-echo "Now you're fishing with style"
 set -gx TERMINAL gnome-terminal
-
-#set PATH $PATH "~/.local/arduino-1.8.5/"
-#set PATH $PATH "~/.local/LSP/cquery/build/release/bin/"
 
 source (conda info --root)/etc/fish/conf.d/conda.fish
