562d4469545d12344689fa64c4fc1505e13bab3a
Hristo Filaretov <hristo.filaretov@ipk.fraunhofer.de>
2018-08-06 13:27:09 +0200

    emacs!

---
+++ emacs/.emacs.d/init.el
@@ -0,0 +1,30 @@
+;; Look, mom, I'm lisping!
+(require 'package)
+
+(add-to-list 'package-archives
+	     '("melpa" . "https://melpa.org/packages/") t)
+
+(package-initialize)
+
+(eval-when-compile
+  (require 'use-package))
+
+(use-package evil
+  :config
+  (evil-mode 1))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(menu-bar-mode nil)
+ '(package-selected-packages (quote (evil use-package)))
+ '(scroll-bar-mode nil)
+ '(tool-bar-mode nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
