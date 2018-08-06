a43939056bb68a736edbb74852952fcc42445b61
Hristo Filaretov <hristo.filaretov@ipk.fraunhofer.de>
2018-08-06 15:57:32 +0200

    I should learn how to properly vc emacs

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -18,10 +18,13 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" default)))
  '(menu-bar-mode nil)
+ '(package-selected-packages (quote (evil use-package)))
  '(scroll-bar-mode nil)
  '(tool-bar-mode nil)
- '(package-selected-packages (quote (evil use-package)))
  '(vc-follow-symlinks t))
 
 (custom-set-faces
@@ -30,3 +33,5 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+(load-theme 'solarized-light t)
