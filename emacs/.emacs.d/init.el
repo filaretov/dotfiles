cd413ffd76f17a9f0b7c759147b29046d80bf576
Hristo Filaretov <hristo.filaretov@ipk.fraunhofer.de>
2018-08-07 16:49:07 +0200

    emacs: be good and add packages

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -11,7 +11,19 @@
 
 (use-package evil
   :config
-  (evil-mode 1))
+  (evil-mode 0))
+
+(use-package which-key
+  :config
+  (which-key-mode))
+
+(use-package magit
+  :ensure t)
+
+(use-package re-builder
+  :config
+  (setq reb-re-syntax 'string)
+  :bind (("C-c R" . re-builder)))
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -21,8 +33,9 @@
  '(custom-safe-themes
    (quote
     ("d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" default)))
+ '(default-input-method "TeX")
  '(menu-bar-mode nil)
- '(package-selected-packages (quote (evil use-package)))
+ '(package-selected-packages (quote (magit which-key evil use-package)))
  '(scroll-bar-mode nil)
  '(tool-bar-mode nil)
  '(vc-follow-symlinks t))
@@ -35,3 +48,21 @@
  )
 
 (load-theme 'solarized-light t)
+
+(ido-mode 1)
+(global-hl-line-mode 1)
+(setq ido-enable-flex-matching t
+      ido-everywhere t)
+
+(setq inhibit-startup-message t
+      inhibit-startup-echo-area-message t)
+
+(setq eshell-visual-commands '(top))
+
+(setq next-line-add-newlines t
+      enable-recursive-minibuffers t)
+
+(add-hook 'after-save-hook
+	  'executable-make-buffer-file-executable-if-script-p)
+
+(define-key global-map (kbd "RET") 'newline-and-indent)
