b1a30f84d755e44bec7d9ad22a8db4e5fa4f757b
Hristo Filaretov <h.filaretov@protonmail.com>
2018-08-12 15:39:28 +0200

    emacs: config update

---
+++ emacs/.emacs.d/custom.el
@@ -0,0 +1,25 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("aaffceb9b0f539b6ad6becb8e96a04f2140c8faa1de8039a343a4f1e009174fb" "ef98b560dcbd6af86fbe7fd15d56454f3e6046a3a0abd25314cfaaefd3744a9e" "c856158cc996d52e2f48190b02f6b6f26b7a9abd5fea0c6ffca6740a1003b333" "7d2e7a9a7944fbde74be3e133fc607f59fdbbab798d13bd7a05e38d35ce0db8d" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" default)))
+ '(default-input-method "TeX")
+ '(fill-column 79)
+ '(menu-bar-mode nil)
+ '(org-agenda-files (quote ("~/Documents/journal/learning-emacs.org")))
+ '(package-selected-packages
+   (quote
+    (htmlize pdf-tools whole-line-or-region markdown-mode magit which-key evil use-package)))
+ '(scroll-bar-mode nil)
+ '(tool-bar-mode nil)
+ '(vc-follow-symlinks t))
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
