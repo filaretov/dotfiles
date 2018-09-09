6731b7da6abbe8529603b81c884a4641b8a4a273
Hristo Filaretov <h.filaretov@protonmail.com>
2018-09-09 19:12:29 +0200

    emacs

--- emacs/.emacs.d/custom.el
+++ emacs/.emacs.d/custom.el
@@ -5,16 +5,13 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
-    ("9240e71034689655a6c05c04063af2c90d0a831aa4e7ca24c8b6e29b5a2da946" "aaffceb9b0f539b6ad6becb8e96a04f2140c8faa1de8039a343a4f1e009174fb" "ef98b560dcbd6af86fbe7fd15d56454f3e6046a3a0abd25314cfaaefd3744a9e" "c856158cc996d52e2f48190b02f6b6f26b7a9abd5fea0c6ffca6740a1003b333" "7d2e7a9a7944fbde74be3e133fc607f59fdbbab798d13bd7a05e38d35ce0db8d" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" default)))
+    ("eacee5f1c2f46185d6f089d97b7953bd9fc5d5b773f72cc76c9fcb556eeba174" "9240e71034689655a6c05c04063af2c90d0a831aa4e7ca24c8b6e29b5a2da946" "aaffceb9b0f539b6ad6becb8e96a04f2140c8faa1de8039a343a4f1e009174fb" "ef98b560dcbd6af86fbe7fd15d56454f3e6046a3a0abd25314cfaaefd3744a9e" "c856158cc996d52e2f48190b02f6b6f26b7a9abd5fea0c6ffca6740a1003b333" "7d2e7a9a7944fbde74be3e133fc607f59fdbbab798d13bd7a05e38d35ce0db8d" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" default)))
  '(default-input-method "TeX")
  '(fill-column 79)
- '(menu-bar-mode nil)
  '(org-agenda-files (quote ("~/Documents/journal/learning-emacs.org")))
  '(package-selected-packages
    (quote
-    (comment-dwim-2 rich-minority nova-theme yaml-mode solarized-theme nord-theme htmlize pdf-tools whole-line-or-region markdown-mode magit which-key evil use-package)))
- '(scroll-bar-mode nil)
- '(tool-bar-mode nil)
+    (evil-surround poet-theme comment-dwim-2 rich-minority nova-theme yaml-mode solarized-theme nord-theme htmlize pdf-tools whole-line-or-region markdown-mode magit which-key evil use-package)))
  '(vc-follow-symlinks t))
 
 (custom-set-faces
