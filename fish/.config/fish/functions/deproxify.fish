e41c334645d0fa36156308de45c7ed8054d5bb01
Hristo Filaretov <h.filaretov@protonmail.com>
2018-09-24 10:17:32 +0200

    proxier

--- fish/.config/fish/functions/deproxify.fish
+++ fish/.config/fish/functions/deproxify.fish
@@ -3,5 +3,9 @@ function deproxify -d "Remove Fraunhofer IPK env vars"
     set -ex https_proxy
     set -ex http_proxy
     set -ex ftp_proxy
+    set -ex ALL_PROXY
+    set -ex HTTPS_PROXY
+    set -ex HTTP_PROXY
+    set -ex FTP_PROXY
     set -ex no_proxy
 end
