e3551f868eb63d3f3b7459591409341a9cfad246
Hristo Filaretov <h.filaretov@protonmail.com>
2018-09-30 18:03:53 +0200

    hippie expand

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -134,6 +134,19 @@
 (add-hook 'after-save-hook
 	  'executable-make-buffer-file-executable-if-script-p)
 
+;; ** Completion
+;; hippie expand is dabbrev expand on steroids
+(setq hippie-expand-try-functions-list '(try-expand-dabbrev
+					 try-expand-dabbrev-all-buffers
+					 try-expand-dabbrev-from-kill
+					 try-complete-file-name-partially
+					 try-complete-file-name
+					 try-expand-all-abbrevs
+					 try-expand-list
+					 try-expand-line
+					 try-complete-lisp-symbol-partially
+					 try-complete-lisp-symbol))
+
 ;; * Major mode configuration
 ;; ** Org mode
 (add-hook 'org-mode-hook (lambda () (progn
