cac613fa967c312e1413794f821388ab1369a7ad
Hristo Filaretov <h.filaretov@protonmail.com>
2018-10-07 14:34:12 +0200

    fish prompt

--- fish/.config/fish/functions/fish_prompt.fish
+++ fish/.config/fish/functions/fish_prompt.fish
@@ -36,10 +36,11 @@ function fish_prompt --description 'Write out the prompt'
 
     # Git prompt settings
     set -l git_repo (git rev-parse --is-inside-work-tree ^/dev/null)
-    if test "$git_repo" = "true"
+    # git repo exists and EMACS string is zero-length
+    if test "$git_repo" = "true" -a -z "$EMACS"
         echo -n -s (set_color blue) (basename (git rev-parse --show-toplevel)) $normal
-        echo -n -s "->" (__fish_git_prompt "%s")
-        #echo -n -s (set_color green) (__fish_git_prompt_informative_status) $normal " "
+        echo -n -s '->' (__fish_git_prompt "%s")
+        # echo -n -s (set_color green) (__fish_git_prompt_informative_status) $normal " "
     end
     # PWD
     echo -n -s "(" (prompt_pwd) ")" " "
