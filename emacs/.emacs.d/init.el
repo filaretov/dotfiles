2bfe949505044c4f5469b30ec4b94ccce4b7443e
Hristo Filaretov <h.filaretov@protonmail.com>
2018-12-03 09:38:56 +0100

    buncha stuff

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -239,6 +239,26 @@
 (setq org-src-fontify-natively t
       org-src-tab-acts-natively t)
 
+;; *** Publishing
+(setq org-publish-project-alist
+      '(("org-notes"
+	 :base-directory "~/Documents/blog/org/"
+	 :base-extension "org"
+	 :publishing-directory "~/Documents/blog/public_html/"
+	 :recursive t
+	 :publishing-function org-html-publish-to-html
+	 :headline-levels 4
+	 :auto-preamble t
+	 )
+	("org-static"
+	 :base-directory "~/Documents/blog/org/"
+	 :base-extension "css\\|js\\|png\\|jpg\\|gif\\|pdf\\|mp3\\|ogg\\|swf"
+	 :publishing-directory "~/Documents/blog/public_html/"
+	 :recursive t
+	 :publishing-function org-publish-attachment
+	 )
+	(" org" :components ("org-notes" "org-static"))))
+
 ;; ** Tex mode
 (use-package tex
   :defer t
@@ -333,6 +353,8 @@
 (add-hook 'rust-mode-hook 'racer-mode)
 (add-hook 'rust-mode-hook 'cargo-minor-mode)
 
+;; ** Yaml
+(use-package yaml-mode)
 ;; * Minor mode configuration
 ;; ** Outline-minor
 ;; *** Init
@@ -504,6 +526,7 @@ Repeated invocations toggle between the two most recently open buffers."
 (global-set-key [remap evil-previous-line] 'evil-previous-visual-line)
 
 ;; ** Hydras
+(use-package hydra)
 (defhydra hydra-window ()
   "Window management" 
   ("h" evil-window-left "left")
