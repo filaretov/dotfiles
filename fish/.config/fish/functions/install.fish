02a457ae8e7a5225a69e5cb30a3e964b188c062b
Hristo Filaretov <h.filaretov@protonmail.com>
2019-02-24 10:17:20 +0100

    extend fish install to buntu

--- fish/.config/fish/functions/install.fish
+++ fish/.config/fish/functions/install.fish
@@ -1,5 +1,9 @@
 function install --wraps dnf -d "Package manager wrapper"
+    set -l cmd "sudo dnf install"
+    if string match -q -- "*Ubuntu*" (uname -a)
+        set cmd "sudo apt install"
+    end
     set -l log_file ~/Documents/cloud/Log/installed
     echo $argv >> $log_file
-    sudo dnf install $log_file
+    eval "$cmd $argv"
 end
--- fish/.config/fish/ridcully.fish
+++ fish/.config/fish/ridcully.fish
@@ -3,7 +3,6 @@ if test -f $HOME/.miniconda3/etc/fish/conf.d/conda.fish
     source $HOME/.miniconda3/etc/fish/conf.d/conda.fish
 end
 
-alias install "sudo apt install"
 alias search "apt search"
 alias remove "apt remove"
 
