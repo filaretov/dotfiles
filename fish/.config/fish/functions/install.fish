b490523e54ac22f4ee4368a77fe5c9cadde25536
Hristo Filaretov <h.filaretov@protonmail.com>
2019-03-09 20:49:06 +0100

    change some stuff

--- fish/.config/fish/functions/install.fish
+++ fish/.config/fish/functions/install.fish
@@ -1,9 +1,6 @@
-function install --wraps dnf -d "Package manager wrapper"
+function install --wraps dnf -d "Package installation wrapper"
     set -l cmd "sudo dnf install"
-    if string match -q -- "*Ubuntu*" (uname -a)
-        set cmd "sudo apt install"
-    end
-    set -l log_file ~/Documents/cloud/Log/installed
+    set -l log_file ~/Documents/Cloud/Log/installed
     echo $argv >> $log_file
     eval "$cmd $argv"
 end
--- fish/.config/fish/ridcully.fish
+++ fish/.config/fish/ridcully.fish
@@ -3,8 +3,5 @@ if test -f $HOME/.miniconda3/etc/fish/conf.d/conda.fish
     source $HOME/.miniconda3/etc/fish/conf.d/conda.fish
 end
 
-alias search "apt search"
-alias remove "apt remove"
-
 add-path "$HOME/.cargo/bin"
 add-path "$HOME/.local/bin"
