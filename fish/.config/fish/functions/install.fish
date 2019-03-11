213d3ba95e42443e64c52fe13794841e7378a95f
Hristo Filaretov <h.filaretov@protonmail.com>
2019-03-11 17:45:19 +0100

    don't repeat work

--- fish/.config/fish/functions/install.fish
+++ fish/.config/fish/functions/install.fish
@@ -1,6 +1,3 @@
 function install --wraps dnf -d "Package installation wrapper"
-    set -l cmd "sudo dnf install"
-    set -l log_file ~/Documents/Cloud/Log/installed
-    echo $argv >> $log_file
-    eval "$cmd $argv"
+    sudo dnf install $argv
 end
