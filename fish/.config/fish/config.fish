411d03f470312f889f7fbbda971bf0d0a9d59416
Hristo Filaretov <h.filaretov@protonmail.com>
2019-05-29 11:07:38 +0200

    aliases

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,6 +2,8 @@ set  fish_greeting "Don't panic!"
 
 alias search "dnf search"
 alias remove "sudo dnf remove"
+alias clk-in "echo i (date +\"%Y/%m/%d %H:%M\") Work:Fraunhofer IPK >> ~/.journal/ipk.time"
+alias clk-out "echo o (date +\"%Y/%m/%d %H:%M\") >> ~/.journal/ipk.time"
 
 # Find local fish file
 set -l lc_file "$HOME/.config/fish/"(hostname)".fish"
