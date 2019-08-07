3bbff4b381a6c3d0c340823b4a443758304ab5d2
Hristo Filaretov <h.filaretov@protonmail.com>
2019-08-07 11:02:51 +0200

    remove junk files

--- bin/.local/bin/stowed/pwgen_to_clip.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# SPDX-FileCopyrightText: 2019 Hristo Filaretov <h.filaretov@campus.tu-berlin.de>
-# SPDX-License-Identifier: MIT
-# xsel didn't work for some reason
-pwgen 64 1 | xclip -sel clip
-notify-send "Password copied to clipboard" -t 750
