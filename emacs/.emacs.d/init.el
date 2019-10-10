3cf0b21c3735143f0518939aa03fd2159b7eb916
Hristo Filaretov <h.filaretov@campus.tu-berlin.de>
2019-10-10 09:22:18 +0200

    freaking defaults

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -13,7 +13,7 @@
 
 (defun emacs.d (filename)
   "Return the complete file path."
-  (format "%s/%s" user-emacs-directory filename))
+  (format "%s%s" user-emacs-directory filename))
 
 (defun journal.d (filename)
   "Return complete path."
