c91feb00b6410e8179194ac7f5fe636dbee2274b
Hristo Filaretov <h.filaretov@campus.tu-berlin.de>
2019-10-22 17:56:27 +0200

    nvim: clean up packaging

--- nvim/.config/nvim/install_minpac.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# SPDX-FileCopyrightText: 2019 Hristo Filaretov <h.filaretov@campus.tu-berlin.de>
-# SPDX-License-Identifier: MIT
-git clone https://github.com/k-takata/minpac.git ~/.config/nvim/pack/minpac/opt/minpac
