f060a52e03426bfc79ae89ed19ca5d683ffcc747
Hristo Filaretov <h.filaretov@campus.tu-berlin.de>
2019-10-27 22:27:04 +0100

    lots of small changes

--- qtile/.config/qtile/config.py
+++ qtile/.config/qtile/config.py
@@ -1,8 +1,14 @@
+# vim: set foldmethod=marker:
+# Imports {{{
 from libqtile.config import Key, Screen, Group, Drag, Click
 from libqtile.lazy import lazy
 from libqtile import layout, bar, widget
 
 from typing import List
+
+# }}}
+
+# Keys {{{
 
 alt = "mod1"
 hyper = "mod3"
@@ -10,34 +16,58 @@ mod = "mod4"
 
 keys = [
     # Switch between windows in current stack pane
-    Key([mod], "k", lazy.layout.down()),
-    Key([mod], "j", lazy.layout.up()),
-    # Move windows up or down in current stack
-    Key([mod, "control"], "k", lazy.layout.shuffle_down()),
-    Key([mod, "control"], "j", lazy.layout.shuffle_up()),
+    Key([mod], "j", lazy.layout.down()),
+    Key([mod], "k", lazy.layout.up()),
+    Key([mod], "h", lazy.layout.left()),
+    Key([mod], "l", lazy.layout.right()),
+    Key([mod, "shift"], "j", lazy.layout.shuffle_down()),
+    Key([mod, "shift"], "k", lazy.layout.shuffle_up()),
+    Key([mod, "shift"], "h", lazy.layout.shuffle_left()),
+    Key([mod, "shift"], "l", lazy.layout.shuffle_right()),
+    Key([mod, "control"], "j", lazy.layout.grow_down()),
+    Key([mod, "control"], "k", lazy.layout.grow_up()),
+    Key([mod, "control"], "h", lazy.layout.grow_left()),
+    Key([mod, "control"], "l", lazy.layout.grow_right()),
     # Switch window focus to other pane(s) of stack
-    Key([mod], "l", lazy.layout.next()),
     # Swap panes of split stack
     Key([mod, "shift"], "space", lazy.layout.rotate()),
+
     # Toggle between split and unsplit sides of stack.
     # Split = all windows displayed
     # Unsplit = 1 window displayed, like Max layout, but still with
     # multiple stack panes
     Key([mod, "shift"], "Return", lazy.layout.toggle_split()),
+
     # Toggle between different layouts as defined below
     Key([mod], "Tab", lazy.next_layout()),
-    Key([mod], "w", lazy.window.kill()),
-    Key([mod, "control"], "r", lazy.restart()),
-    Key([mod, "control"], "q", lazy.shutdown()),
+    Key([mod], "q", lazy.window.kill()),
+    Key([mod, "shift"], "r", lazy.restart()),
+    Key([mod, "shift"], "q", lazy.shutdown()),
     Key([mod], "r", lazy.spawncmd()),
+
     # Launch programs
     Key([mod], "e", lazy.spawn("terminator -e ranger")),
+    Key([mod], "p", lazy.spawn("/home/hgf/.local/bin/stowed/pass_rofi.sh")),
     Key([mod], "Return", lazy.spawn("terminator")),
     Key([mod], "space", lazy.spawn("rofi -show drun")),
     Key([alt], "space", lazy.spawn("rofi -show drun")),
 ]
 
+# }}}
+
+# Groups {{{
 groups = [Group(i) for i in "1234567890"]
+# groups = [
+#     Group(i)
+#     for i in [
+#         "1: term",
+#         "2: edit",
+#         "3: file",
+#         "8: docs",
+#         "9: chat",
+#         "0: web",
+#     ]
+# ]
 
 for i in groups:
     keys.extend(
@@ -52,10 +82,14 @@ for i in groups:
         ]
     )
 
+# }}}
+
+# Layouts {{{
 layouts = [
+    layout.Columns(),
     layout.Max(),
-    layout.Stack(num_stacks=3),
-    # Try more layouts by unleashing below layouts.
+    # layout.Stack(num_stacks=2),
+    # layout.Stack(num_stacks=3),
     # layout.Bsp(),
     # layout.Columns(),
     # layout.Matrix(),
@@ -68,17 +102,21 @@ layouts = [
     # layout.Zoomy(),
 ]
 
-widget_defaults = dict(font="Source Code Pro", fontsize=13, padding=0)
+# }}}
+
+# Widgets and screens {{{
+widget_defaults = dict(font="Source Code Pro", fontsize=12, padding=10)
 extension_defaults = widget_defaults.copy()
 
 screens = [
     Screen(
-        bottom=bar.Bar(
+        top=bar.Bar(
             [
                 widget.CurrentLayout(),
                 widget.GroupBox(),
                 widget.Prompt(),
-                widget.WindowName(),
+                widget.Spacer(),
+                # widget.WindowName(),
                 widget.Systray(),
                 widget.Clock(format="%Y-%m-%d %a %H:%M"),
             ],
@@ -87,6 +125,9 @@ screens = [
     )
 ]
 
+# }}}
+
+# Mouse {{{
 # Drag floating layouts.
 mouse = [
     Drag(
@@ -100,6 +141,10 @@ mouse = [
     ),
     Click([mod], "Button2", lazy.window.bring_to_front()),
 ]
+
+# }}}
+
+# Misc {{{
 
 dgroups_key_binder = None
 dgroups_app_rules = []  # type: List
@@ -128,6 +173,9 @@ floating_layout = layout.Floating(
 auto_fullscreen = True
 focus_on_window_activation = "smart"
 
+# }}}
+
+# wmname {{{
 # XXX: Gasp! We're lying here. In fact, nobody really uses or cares about this
 # string besides java UI toolkits; you can see several discussions on the
 # mailing lists, GitHub issues, and other WM documentation that suggest setting
@@ -137,3 +185,4 @@ focus_on_window_activation = "smart"
 # We choose LG3D to maximize irony: it is a 3D non-reparenting WM written in
 # java that happens to be on java's whitelist.
 wmname = "LG3D"
+# }}}
