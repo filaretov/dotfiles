fa4c2fd4c54d01e3df32cab0fc57a97343922543
Hristo Filaretov <h.filaretov@campus.tu-berlin.de>
2020-01-26 12:05:43 +0100

    fish: dirty changes

--- fish/.config/fish/functions/dirty.fish
+++ fish/.config/fish/functions/dirty.fish
@@ -3,15 +3,29 @@ function dirty
     cd ~/dev
     set dirs (fdfind --absolute-path --max-depth 3 --type --type d --hidden '.git$')
 
-    for d in $dirs
-        cd $d/..
-        set git_status (git status --porcelain)
-        if test -n "$git_status"
-            if test "$argv[1]" = "cd"
-                cd (string replace -a '.git' '' "$d")
+    for _d in $dirs
+        set d (string replace '/.git' '' "$_d")
+        cd $d
+        if contains "fetch" $argv
+            git fetch
+        end
+        if contains "pull" $argv
+            git pull
+        end
+        set what (git status --porcelain=1)
+        if test -n "$what"
+            echo $what
+            set git_dirty "*"
+            return 0
+        end
+        set git_ab (git status --porcelain=1 --branch | grep -o '\[ahead.*\]\|\[behind.*\]')
+        if test -n "$git_dirty" -o -n "$git_ab"
+            echo $git_dirty $git_ab
+            if contains "cd" $argv
+                cd $d
                 return 0
             end
-            echo (string replace "$HOME" '' (string replace -a '/.git' '' "$d"))
+            echo (string replace "$HOME" '' $d) $git_dirty $git_ab
         end
     end
 
