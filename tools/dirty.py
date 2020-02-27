84699d8074984160dcd74b735ee1b97bef675b56
Hristo Filaretov <h.filaretov@campus.tu-berlin.de>
2020-02-27 11:59:07 +0100

    update dirty tool

--- tools/dirty.py
+++ tools/dirty.py
@@ -1,9 +1,22 @@
 import git
 import typer
-from pathlib import Path
+import pathlib
 from typing import List
 
 app = typer.Typer()
+
+
+def find_git_repos(path: pathlib.Path, levels: int):
+    if levels == 0:
+        return []
+    repos = []
+    dirs = (p for p in path.iterdir() if p.is_dir())
+    for p in dirs:
+        if p.joinpath(".git").exists():
+            repos.append(p)
+        else:
+            repos += find_git_repos(p, levels - 1)
+    return repos
 
 
 def is_clean(repo: git.Repo):
@@ -19,8 +32,8 @@ def is_clean(repo: git.Repo):
 
 
 @app.command()
-def check(repo_paths: List[Path]):
-    for repo_path in repo_paths:
+def check(top: pathlib.Path):
+    for repo_path in find_git_repos(top, 3):
         try:
             repo = git.Repo(repo_path)
             is_clean(repo)
