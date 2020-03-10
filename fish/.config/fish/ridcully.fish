30cdff86bb5d5576f3258ca991a848e974265475
Hristo Filaretov <h.filaretov@campus.tu-berlin.de>
2020-03-10 15:18:09 +0100

    cargo bin

--- fish/.config/fish/ridcully.fish
+++ fish/.config/fish/ridcully.fish
@@ -5,6 +5,7 @@ if test -f $HOME/.miniconda3/etc/fish/conf.d/conda.fish
     source $HOME/.miniconda3/etc/fish/conf.d/conda.fish
 end
 
+set -x PATH ~/.cargo/bin $PATH
 set -x PATH ~/.local/bin $PATH
 set -x PATH ~/.local/bin/stowed $PATH
 set -x PATH ~/dev/go/bin $PATH
